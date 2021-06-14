e4dc9900c937d1cdf03cc0b43f5b7e2b21ba9f1d
Prashant Yadav <prashantyadav@Prashants-MacBook-Air.local>
2021-06-14 19:07:38 +0530

    slide works

--- src/components/Slider/ImageSlider.js
+++ src/components/Slider/ImageSlider.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react'
 import { SliderData } from './SliderData'
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+// import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+// import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import NavigateNextSharpIcon from '@material-ui/icons/NavigateNextSharp';
+import NavigateBeforeSharpIcon from '@material-ui/icons/NavigateBeforeSharp';
 
 const ImageSlider = ({ slides }) => {
 
     const [current, setCurrent] = useState(0)
     const length = slides.length
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current - 1);
+    }
+    console.log(current)
+
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null
+    }
+
+
+
     return (
         <section className="slider">
-            <KeyboardArrowLeftIcon className="left-arrow" />
-            <KeyboardArrowRightIcon className="right-arrow" />
+            {/* <h1>hello</h1> */}
+            <NavigateBeforeSharpIcon className="left-arrow" onClick={prevSlide} />
+            <NavigateNextSharpIcon className="right-arrow" onClick={nextSlide} />
             {SliderData.map((slide, index) => {
-                return <img src={slide.image} alt="work" />
+                return (
+                    <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                        {index === current && <img src={slide.image} alt="work" className="image" />}
+                    </div>
+                )
             })}
         </section>
     )
