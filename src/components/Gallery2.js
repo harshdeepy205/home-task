72c4aee5a8564040f1606961b452337653043bb7
Aniruddh Parwal <aniruddhparwal@gmail.com>
2021-07-12 21:43:24 +0530

    responsive image

--- src/components/Gallery2.js
+++ src/components/Gallery2.js
@@ -9,12 +9,13 @@ function Gallery2() {
                 <Row className="rows" style={{ marginTop: '1rem' }}>
                     <Col md={4}>
                         <div className="box1">
-                            hello
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
                         </div>
                     </Col>
                     <Col md={8}>
                         <div className="box2">
-                            bye
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                     </Col>
                 </Row>
@@ -22,14 +23,17 @@ function Gallery2() {
                 <Row>
                     <Col md={6} className="column">
                         <div className="col-box-1" style={{ height: '50vh', width: '100%', background: 'grey', marginTop: '0.5rem' }}>
-                            img1
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                         <div className="col-box-1" style={{ width: '100%', height: '100vh', marginTop: '0.5rem', background: 'green' }}>
-                            img2
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                             {/* <img style={{ objectFit: 'contain', width: '100%', height: '100vh' }} src="https://images.pexels.com/photos/2419375/pexels-photo-2419375.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> */}
                         </div>
                         <div className="col-box-1" style={{ height: '50vh', width: '100%', background: 'grey', marginTop: '0.5rem' }}>
-                            img3
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                         {/* <img src="https://images.pexels.com/photos/3586966/pexels-photo-3586966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <img src="https://images.pexels.com/photos/2419375/pexels-photo-2419375.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
@@ -37,10 +41,12 @@ function Gallery2() {
                     </Col>
                     <Col md={6} className="column">
                         <div className='col2-box' style={{ height: '100vh', marginTop: '0.5rem', width: '100%', background: 'green' }}>
-                            img1
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                         <div className='col2-box' style={{ height: '101vh', marginTop: '0.5rem', background: 'grey' }}>
-                            img2
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                         {/* <img src="https://images.pexels.com/photos/2019546/pexels-photo-2019546.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                         <img src="https://images.pexels.com/photos/1980720/pexels-photo-1980720.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> */}
@@ -49,12 +55,14 @@ function Gallery2() {
                 <Row style={{ marginTop: '0.5rem' }}>
                     <Col md={4}>
                         <div className="box1">
-                            hello
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                     </Col>
                     <Col md={8}>
                         <div className="box2">
-                            bye
+                            <img style={{ height: '100%', width: '100%' }} src="https://picsum.photos/200" />
+
                         </div>
                     </Col>
                 </Row>
